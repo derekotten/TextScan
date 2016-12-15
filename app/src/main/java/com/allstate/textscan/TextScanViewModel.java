8d643eb762df25fa427952dcfdc59dade9e39bd7
Derek <dottg@allstate.com>
2016-12-14 18:15:38 -0600

    adding authorize phone number capability to view model

--- app/src/main/java/com/allstate/textscan/TextScanViewModel.java
+++ app/src/main/java/com/allstate/textscan/TextScanViewModel.java
@@ -3,6 +3,10 @@ package com.allstate.textscan;
 
 import javax.inject.Inject;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TextScanViewModel {
 
     @Inject BlacklistApiClient blacklistApiClient;
@@ -11,5 +15,17 @@ public class TextScanViewModel {
         Injector.getInjector().inject(this);
     }
 
-    
+    void authorizePhoneNumber(String phoneNumber) {
+        blacklistApiClient.getNumberAuthorization(phoneNumber).enqueue(new Callback<PhoneNumberAuthorization>() {
+            @Override
+            public void onResponse(Call<PhoneNumberAuthorization> call, Response<PhoneNumberAuthorization> response) {
+                System.out.println(response.body().message);
+            }
+
+            @Override
+            public void onFailure(Call<PhoneNumberAuthorization> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
+    }
 }
